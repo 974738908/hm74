769bfd0d4c2e7942996f3d6534623e707689f575
cx <974738908@qq.com>
2019-07-11 21:13:00 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <!-- 一级路由对应组件渲染的位置 -->
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import App from './App.vue'
 
 import ELEMENT from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+// 1.默认索引 文件夹下存在 index.js index.vue index.json 默认加载这些文件
+// 2.在使用vue-cli的时候 @符号在路径中作为前缀使用的时候 代表src
+import router from '@/router'
+
+// 导入axios
+import axios from 'axios'
+Vue.prototype.axios = axios
+
 Vue.use(ELEMENT)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
